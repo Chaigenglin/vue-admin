3740ee0ef8bc90406d86dc5ad795b6e83c096376
cgl <cgl981125@163.com>
2022-04-25 23:50:07 +0800

    feat: 页面布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    home123
+  <div class="basic-layout">
+    <div class="nav-side"></div>
+    <div class="content-right">
+      <div class="nav-top">
+        <div class="bread">面包屑</div>
+        <div class="user">用户</div>
+      </div>
+      <div class="wrapper">
+        <div class="main-page">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
   <router-view></router-view>
 </template>
@@ -11,6 +22,38 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.basic-layout {
+  position: relative;
+  .nav-side {
+    position: fixed;
+    width: 200px;
+    height: 100vh;
+    background-color: #001925;
+    color: #fff;
+    overflow-y: auto;
+    transition: width .5s;
+  }
+  .content-right {
+    margin-left: 200px;
+    .nav-top {
+      height: 50px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 1px solid #ddd;
+      align-items: center;
+      padding: 0 20px;
+    }
+    .wrapper {
+      background-color: #eef0f3;
+      padding: 20px;
+      height: calc(100vh - 50px);
+      .main-page {
+        height: 100%;
+        background-color: #fff;
+      }
+    }
+  }
+}
 
 </style>
